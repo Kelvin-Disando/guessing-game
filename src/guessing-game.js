d8df5637c31bfb4dbca8db34630b99834a7bb313
Denis Danilkov <kvantokost@gmail.com>
2019-10-08 13:18:32 +0300

    do all tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessingArray = [];
+        this.guessNumber = 0;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        for (let index = min; index <= max; index++) {
+            this.guessingArray.push(index);
+        }
+        this.min = 0;
+        this.max = this.guessingArray.length - 1;
     }
 
     guess() {
-
+        this.guessNumber = Math.round((this.min + this.max) / 2);
+        return this.guessingArray[this.guessNumber];
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
